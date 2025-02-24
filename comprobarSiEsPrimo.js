22dfc1d9f56e6a3cc19ca2d56a06e6d02079b19a
xwan1111 <xwan1111@g.educaand.es>
2025-02-24 12:36:03 +0100

    Update comprobarSiEsPrimo.js

--- comprobarSiEsPrimo.js
+++ comprobarSiEsPrimo.js
@@ -1,18 +1,18 @@
 function comprobarEsPrimo (numero)
-  {
-    var esPrimo;
-    var i;
+{
+  var esPrimo;
+  var i;
 
-for (i=numero-1; i>=2; i=i-1 )
-  {
-        if (numero % i == 0)
-            {
-            esPrimo = false;
-            return esPrimo;
-            }
-  }
-    // si no hemos encontrado ningun divisor entonces el numero es primo
-    esPrimo = true;
-    return esPrimo;
-  }
+  for (i=numero-1; i>=2; i=i-1 )
+    {
+      if ( numero % i == 0 )
+        {
+        esPrimo = false;
+        return esPrimo;
+        }
+    }
+// si no hemos encontrado ningún divisor entonces el numero es primo
+esPrimo = true;  
+return esPrimo;
+}
     
