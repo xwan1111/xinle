ff6530a11fbf6643e0604c071e4233fb235e69df
xwan1111 <xwan1111@g.educaand.es>
2025-03-17 12:37:38 +0100

    Update pedirNumero.js

--- pedirNumero.js
+++ pedirNumero.js
@@ -1,4 +1,15 @@
-function pedirNumero(min, max) {
+function pedirNumero (min, max) 
+{
+  var numero;
+  do {
+    numero = prompt("Ingrese un número entre " + min +" y " + max + ":");
+    numero = Number(numero); 
+  } while (numero < min || numero > max);
+
+  return numero;
+}
+
+function pedirNumeroMejorado (min, max) {
   var numero;
   do {
     numero = prompt("Ingrese un número entre " + min +" y " + max + ":");
