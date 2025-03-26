7f0e887b23c1d4ba9c5d14446250e81c49b22d5d
xwan1111 <xwan1111@g.educaand.es>
2025-03-26 13:03:11 +0100

    Update pedirNumero.js

--- pedirNumero.js
+++ pedirNumero.js
@@ -10,11 +10,16 @@ function pedirNumero (min, max)
 }
 
 function pedirNumeroMejorado (texto,min,max) {
-  var numero;
-  do {
-    numero = prompt(texto + min +" y " + max + ":");
-    numero = Number(numero); 
-  } while (numero < min || numero > max);
-
-  return numero;
+ var numero;
+    var esUnNumero;
+    do {
+        numero = prompt(texto + min + " y " + max + ":");
+        esUnNumero = isNaN(numero);
+        if (esUnNumero) {
+            alert("Entrada no válida. Por favor, ingrese un número de nuevo: ");
+        } else {
+            numero = Number(numero);
+        }
+    } while (esUnNumero || numero < min || numero > max);
+    return numero;
 }
